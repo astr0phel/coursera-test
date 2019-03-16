c814585c057408f45f6a49827fa99fd8f7015230
Abby Smith <30824082+astr0phel@users.noreply.github.com>
2019-03-16 17:35:58 +0900

    update

--- site/module5-solution/js/script.js
+++ site/module5-solution/js/script.js
@@ -104,12 +104,9 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
                 // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
                 // variable's name implies it expects.
-                $ajaxUtils.sendGetRequest(
-                  chosenCategoryShortName,
-                  function (chosenCategoryShortName) {
-                var chosenCategoryShortName = chooseRandomCategory(categories
-                  );
-              }
+                
+                var chosenCategoryShortName = chooseRandomCategory(categories);
+            
               
 
 
@@ -124,13 +121,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // Hint: you need to surround the chosen category short name with something before inserting
                 // it into the home html snippet.
                 //
-                
-                insertProperty(html, "randomCategoryShortName", randomCategoryShortName);
-              dc.loadMenuItems = function() {
-        showLoading("#main-content");
-        $ajaxUtils.sendGetRequest(
-            buildAndShowMenuItemsHTML);
-    }
+                var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName.short_name + "'");
 
                
 
@@ -138,7 +129,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
                 // of how to do that.
-                insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+                insertHtml("#main-content", homeHtml);
 
             },
             false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
